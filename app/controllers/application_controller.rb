1ba50f52db9706baf69a0ffae903b6fe6ab20bc4
daisuke-shimura <daisuke.s0707@icloud.com>
2024-12-04 11:16:25 +0900

    deploy 4 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   USERS = { ENV["USER_NAME"] => ENV["PASSWORD"]}
 
   #before_action :basic_authentication
-  before_action :digest_auth, if: -> { request.path == '/' || request.path == '/manager' || request.path == '/users/sign_in' || request.path == '/users/sign_up' }
+  before_action :digest_auth, if: -> { request.path == '/' || request.path == '/manager' || request.path == '/users/sign_up' }
   before_action :authenticate_user!, except: [:top]
 
   def after_sign_in_path_for(resource)

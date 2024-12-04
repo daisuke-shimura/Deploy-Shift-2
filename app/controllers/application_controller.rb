3d28fcaf5384f1fa3668c4ded37a16578cb672fe
daisuke-shimura <daisuke.s0707@icloud.com>
2024-12-04 11:08:16 +0900

    deploy 更新4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,24 @@ class ApplicationController < ActionController::Base
   USERS = { ENV["USER_NAME"] => ENV["PASSWORD"]}
 
   #before_action :basic_authentication
-  before_action :digest_auth
+  before_action :digest_auth, if: -> { request.path == '/' || request.path == '/manager' || request.path == '/users/sign_in' || request.path == '/users/sign_up' }
   before_action :authenticate_user!, except: [:top]
 
   def after_sign_in_path_for(resource)
   days_path
   end
 
-  def after_sign_out_path_for(resouce)
+  def after_sign_out_path_for(resource)
   root_path
   end
 
+  private
   def digest_auth
     authenticate_or_request_with_http_digest do |user|
       USERS[user]
     end
   end
-  #private
+
   #def basic_authentication
     #authenticate_or_request_with_http_basic do |user_name, password|
       #user_name ==ENV["USER_NAME"] && password == ENV["PASSWORD"]
